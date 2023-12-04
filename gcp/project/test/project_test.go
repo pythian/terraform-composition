60b17ebd2edca1c6b5e175d9a49f43a966440643
Kevin Pedersen <kpedersen@pythian.com>
2023-12-04 15:33:45 -0400

    adds a bunch of tests

--- gcp/project/test/project_test.go
+++ gcp/project/test/project_test.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
+	"github.com/thedevsaddam/gojsonq/v2"
 )
 
 // Flag to destroy the target environment after tests
@@ -142,39 +143,108 @@ func TestGCPProject(t *testing.T) {
 	// Store outputs
 	outputs := terraform.OutputAll(t, terraformOptions)
 
-	// Test project id
-	if assert.NotNil(t, outputs["project_id"]) {
-		t.Logf("Output test PASSED. Expected output to be string, got %s.", outputs["project_id"].(string))
+	// Test billing account
+	if assert.Equal(t, gcp["billing_account"].(string), outputs["billing_account"].(string)) {
+		t.Logf("Billing account test PASSED. Expected billing account id %s, got %s.", gcp["billing_account"].(string), outputs["billing_account"].(string))
 	} else {
-		t.Error("Output test FAILED. Expected output to be string, got nil.")
+		t.Errorf("Billing account test FAILED. Expected billing account id %s, got %s.", gcp["billing_account"].(string), outputs["billing_account"].(string))
 	}
-	if assert.Equal(t, strings.Split(outputs["project_id"].(string), "-")[0], gcp["prefix"]) {
-		t.Logf("Prefix test PASSED. Expected project name to start with %s, got %s.", gcp["prefix"], strings.Split(outputs["project_id"].(string), "-")[0])
+
+	// Test parent folder name
+	if assert.Equal(t, gcp["prefix"].(string), strings.Split(outputs["folder_name"].(string), "-")[0]) {
+		t.Logf("Prefix test PASSED. Expected folder name to start with %s, got %s.", gcp["prefix"], strings.Split(outputs["folder_name"].(string), "-")[0])
 	} else {
-		t.Errorf("Prefix test FAILED. Expected project name to start with %s, got %s.", gcp["prefix"], strings.Split(outputs["project_id"].(string), "-")[0])
+		t.Errorf("Prefix test FAILED. Expected folder name to start with %s, got %s.", gcp["prefix"], strings.Split(outputs["folder_name"].(string), "-")[0])
+	}
+
+	// Test project id
+	if assert.Equal(t, gcp["prefix"].(string), strings.Split(outputs["project_id"].(string), "-")[0]) {
+		t.Logf("Prefix test PASSED. Expected project name to start with %s, got %s.", gcp["prefix"].(string), strings.Split(outputs["project_id"].(string), "-")[0])
+	} else {
+		t.Errorf("Prefix test FAILED. Expected project name to start with %s, got %s.", gcp["prefix"].(string), strings.Split(outputs["project_id"].(string), "-")[0])
 	}
 	if inputs["project"].(map[string]interface{})["random_project_id"].(bool) {
 		if (assert.Len(t, strings.Split(outputs["project_id"].(string), "-"), len(strings.Split(outputs["project_name"].(string), "-"))+1)) &&
 			(assert.Contains(t, outputs["project_id"].(string), outputs["project_name"].(string))) {
-			t.Logf("Suffix test PASSED. Expected random suffix, got %s.", strings.Split(outputs["project_id"].(string), "-")[len(strings.Split(outputs["project_name"].(string), "-"))])
+			t.Logf("Suffix test PASSED. Expected project_id to have a random suffix, got %s.",
+				strings.Split(outputs["project_id"].(string), "-")[len(strings.Split(outputs["project_name"].(string), "-"))])
 		} else {
-			t.Error("Suffix test FAILED. Expected random suffix, got nil.")
+			t.Error("Suffix test FAILED. Expected project_id to have a random suffix, got nil.")
 		}
 	}
 
+	// Test project labels
+	labelFailed := false
+	labels := []string{}
+	for k, v := range env["labels"].(map[string]interface{}) {
+		val, set := outputs["project_labels"].(map[string]interface{})[k].(string)
+		if set {
+			if !assert.Equal(t, v, val) {
+				t.Errorf("Labels test FAILED. Expected label %s to be set to %s, got %s.", k, v, val)
+				labelFailed = true
+			} else {
+				labels = append(labels, k)
+			}
+		} else {
+			t.Errorf("Labels test FAILED. Expected label %s to be set, not found.", k)
+			labelFailed = true
+		}
+	}
+	if !labelFailed {
+		t.Logf("Labels test PASSED. Expected project labels are set: %v", labels)
+	}
+
 	// Test enabled APIs
-	api_failed := false
+	apiFailed := false
 	apis := []string{}
 	for _, api := range inputs["project"].(map[string]interface{})["activate_apis"].([]interface{}) {
 		if !assert.Contains(t, outputs["enabled_apis"].([]interface{}), api) {
 			t.Errorf("APIs test FAILED. Expected API %s to be enabled, not found.", api)
-			api_failed = true
+			apiFailed = true
 		} else {
 			apis = append(apis, api.(string))
 		}
 	}
-	if !api_failed {
+	if !apiFailed {
 		t.Logf("APIs test PASSED. Expected project APIs are enabled: %v", apis)
 	}
 
+	// Get the state in json format
+	moduleJson := gojsonq.New().JSONString(terraform.Show(t, terraformOptions)).From("values.root_module.child_modules").
+		Where("address", "eq", "module.project_factory").
+		Select("child_modules")
+
+	childModules := moduleJson.Get()
+
+	// Traverse the state to locate project values
+	for _, childModule := range childModules.([]interface{})[0].(map[string]interface{})["child_modules"].([]interface{}) {
+		if childModule.(map[string]interface{})["address"].(string) == "module.project_factory.module.project-factory" {
+			for _, resource := range childModule.(map[string]interface{})["resources"].([]interface{}) {
+				if resource.(map[string]interface{})["address"].(string) == "module.project_factory.module.project-factory.google_project.main" {
+					// Test auto-create network
+					if assert.Equal(t, inputs["project"].(map[string]interface{})["auto_create_network"].(bool),
+						resource.(map[string]interface{})["values"].(map[string]interface{})["auto_create_network"].(bool)) {
+						t.Logf("Default network creation test PASSED. Expected auto_create_network set to %t, got %t.",
+							inputs["project"].(map[string]interface{})["auto_create_network"].(bool),
+							resource.(map[string]interface{})["values"].(map[string]interface{})["auto_create_network"].(bool))
+					} else {
+						t.Logf("Default network creation test FAILED. Expected auto_create_network set to %t, got %t.",
+							inputs["project"].(map[string]interface{})["auto_create_network"].(bool),
+							resource.(map[string]interface{})["values"].(map[string]interface{})["auto_create_network"].(bool))
+					}
+					// Test parent folder id
+					if assert.Equal(t, strings.Split(outputs["folder_id"].(string), "/")[1],
+						resource.(map[string]interface{})["values"].(map[string]interface{})["folder_id"].(string)) {
+						t.Logf("Parent folder ID test PASSED. Expected parent folder ID %s, got %s.",
+							strings.Split(outputs["folder_id"].(string), "/")[1],
+							resource.(map[string]interface{})["values"].(map[string]interface{})["folder_id"].(string))
+					} else {
+						t.Logf("Parent folder ID test FAILED. Expected parent folder ID %s, got %s.",
+							strings.Split(outputs["folder_id"].(string), "/")[1],
+							resource.(map[string]interface{})["values"].(map[string]interface{})["folder_id"].(string))
+					}
+				}
+			}
+		}
+	}
 }
