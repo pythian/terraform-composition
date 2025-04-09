db4b7aef153065024f2fdbe0080e89a8154a89df
Daniel Silva <dsilva@pythian.com>
2025-04-09 11:06:46 +0000

    remove current files and redownload the website

--- cnx-website/concrete/elements/summary/templates/default.php
+++
@@ -1,8 +0,0 @@
-<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
-
-<div>
-    <a href="<?=$link?>"><?=$title?></a>
-    <?php if ($description) { ?>
-        <div><?=$description?></div>
-    <?php } ?>
-</div>
