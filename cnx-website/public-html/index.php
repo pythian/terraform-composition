4ed44735805a70a66af8c89b2026c68953436dd8
kpeder <kpeder@users.noreply.github.com>
2025-06-03 16:04:45 -0300

    adding override all; auth seems broken

--- cnx-website/public-html/index.php
+++ cnx-website/public-html/index.php
@@ -7,11 +7,6 @@ ini_set('session.cookie_httponly', 1);
 // Session ID cannot be passed through URLs
 ini_set('session.use_only_cookies', 1);
 
-// Hack for a deployment behind the reverse proxy
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
-    $_SERVER['HTTPS'] = 'on';
-}
-
 // Uses a secure connection (HTTPS) if possible
 ini_set('session.cookie_secure', 1);
 require 'concrete/dispatcher.php';
